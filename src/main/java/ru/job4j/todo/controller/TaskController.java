e85654418e1c233defba4ac63a2ac70ceaa5d644
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-06-13 19:17:29 +0300

    Реализация TaskController и необходимых views

---
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -0,0 +1,108 @@
+package ru.job4j.todo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.todo.dto.TaskDetails;
+import ru.job4j.todo.model.Task;
+import ru.job4j.todo.service.TaskService;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("tasks", taskService.findAll());
+        return "tasks/list";
+    }
+
+    @GetMapping("/done")
+    public String getAllDone(Model model) {
+        model.addAttribute("tasks", taskService.findAllByDone(true));
+        return "tasks/list";
+    }
+
+    @GetMapping("/new")
+    public String getAllNew(Model model) {
+        model.addAttribute("tasks", taskService.findAllByDone(false));
+        return "tasks/list";
+    }
+
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "tasks/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Task task, Model model) {
+        task.setCreated(Timestamp.valueOf(LocalDateTime.now()));
+        Optional<Task> taskOptional = taskService.create(task);
+        if (taskOptional.isEmpty()) {
+            model.addAttribute("message", "При создании задания произошла ошибка");
+            return "errors/409";
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/tasks/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        Optional<TaskDetails> taskOptional = taskService.findById(id);
+        if (taskOptional.isEmpty()) {
+            model.addAttribute("message", "Задание с указанным идентификатором не найдено");
+            return "errors/404";
+        }
+        model.addAttribute("task", taskOptional.get());
+        return "tasks/one";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(Model model, @PathVariable int id) {
+        Optional<TaskDetails> taskOptional = taskService.findById(id);
+        if (taskOptional.isEmpty()) {
+            model.addAttribute("message", "Задание с указанным идентификатором не найдено");
+            return "errors/404";
+        }
+        model.addAttribute("task", taskOptional.get());
+        return "tasks/update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Task task, Model model) {
+        boolean isUpdated = taskService.update(task.getId(), task);
+        if (!isUpdated) {
+            model.addAttribute("message", "При обновлении произошла ошибка");
+            return "errors/409";
+        }
+        return String.format("redirect:/tasks/%d", task.getId());
+    }
+
+    @GetMapping("/done/{id}")
+    public String toDone(Model model, @PathVariable int id) {
+        Optional<TaskDetails> taskOptional = taskService.toDone(id);
+        if (taskOptional.isEmpty()) {
+            model.addAttribute("message", "Задание с указанным идентификатором не найдено");
+            return "errors/404";
+        }
+        model.addAttribute("task", taskOptional.get());
+        return "tasks/one";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        var isDeleted = taskService.delete(id);
+        if (!isDeleted) {
+            model.addAttribute("message", "Задание с указанным идентификатором не найдено");
+            return "errors/404";
+        }
+        return "redirect:/";
+    }
+}
