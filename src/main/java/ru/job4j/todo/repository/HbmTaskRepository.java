05a5c1ed951d2ef887867a79380e9b08bf813426
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-06-13 19:14:29 +0300

    Доработка TaskRepository. Правки в конфигурации Hibernate

--- src/main/java/ru/job4j/todo/repository/HbmTaskRepository.java
+++ src/main/java/ru/job4j/todo/repository/HbmTaskRepository.java
@@ -7,6 +7,7 @@ import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,20 +16,21 @@ public class HbmTaskRepository implements TaskRepository {
     private final SessionFactory sf;
 
     @Override
-    public Task create(Task task) {
-        sf.getCurrentSession();
+    public Optional<Task> create(Task task) {
+        Optional<Task> taskOptional = Optional.empty();
         Session session = sf.openSession();
         try {
             session.beginTransaction();
             int id = (int) session.save(task);
             task.setId(id);
+            taskOptional = Optional.of(task);
             session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
         } finally {
             session.close();
         }
-        return task;
+        return taskOptional;
     }
 
     @Override
@@ -39,13 +41,12 @@ public class HbmTaskRepository implements TaskRepository {
             session.beginTransaction();
             rsl = session.createQuery("""
                         UPDATE Task SET title = :title,
-                        description = :description, created=:created, done=:done
+                        description = :description, done=:done
                         WHERE id = :id
                     """)
                     .setParameter("id", id)
                     .setParameter("title", task.getTitle())
                     .setParameter("description", task.getDescription())
-                    .setParameter("created", task.getCreated())
                     .setParameter("done", task.isDone())
                     .executeUpdate() > 0;
             session.getTransaction().commit();
@@ -55,6 +56,28 @@ public class HbmTaskRepository implements TaskRepository {
             session.close();
         }
         return rsl;
+    }
+
+    @Override
+    public Optional<Task> toDone(int id) {
+        Optional<Task> taskOptional = Optional.empty();
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("UPDATE Task SET done=true WHERE id = :id")
+                    .setParameter("id", id)
+                    .executeUpdate();
+            Query<Task> query = session.createQuery(
+                    "from Task where id=:id", Task.class);
+            query.setParameter("id", id);
+            taskOptional = query.uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return taskOptional;
     }
 
     @Override
@@ -74,6 +97,25 @@ public class HbmTaskRepository implements TaskRepository {
             session.close();
         }
         return rsl;
+    }
+
+    @Override
+    public Optional<Task> findById(int id) {
+        Optional<Task> taskOptional = Optional.empty();
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            Query<Task> query = session.createQuery(
+                    "from Task where id=:id", Task.class);
+            query.setParameter("id", id);
+            taskOptional = query.uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return taskOptional;
     }
 
     @Override
--- src/main/java/ru/job4j/todo/repository/TaskRepository.java
+++ src/main/java/ru/job4j/todo/repository/TaskRepository.java
@@ -1,16 +1,20 @@
 package ru.job4j.todo.repository;
 
 import ru.job4j.todo.model.Task;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository {
 
-    Task create(Task task);
+    Optional<Task> create(Task task);
 
     boolean update(int id, Task task);
 
+    Optional<Task> toDone(int id);
+
     boolean delete(int id);
+
+    Optional<Task> findById(int id);
 
     List<Task> findAll();
 
