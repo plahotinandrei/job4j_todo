3fb8aadf800dced247d2b9c4bb03cc24e23b8650
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-06-20 18:02:56 +0300

    fix sql

--- db/scripts/003_ddl_alter_tasks.sql
+++ db/scripts/003_ddl_alter_tasks.sql
@@ -1,6 +1,6 @@
-ALTER TABLE tasks ADD COLUMN user_id int;
+alter table tasks add column user_id int;
 
-ALTER TABLE tasks
-ADD CONSTRAINT todo_user_user_id_fkey
-FOREIGN KEY (user_id)
-REFERENCES todo_user (id);
+alter table tasks
+add constraint todo_user_user_id_fkey
+foreign key (user_id)
+references todo_user (id);
