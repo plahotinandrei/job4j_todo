3e96d3884e68a00c73a72ada965b724659326974
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-06-25 14:29:15 +0300

    3. LazyInitializationexception fix

--- db/scripts/003_ddl_alter_tasks.sql
+++
@@ -1,9 +0,0 @@
-alter table tasks add column user_id int;
-
-alter table tasks
-add constraint todo_user_user_id_fkey
-foreign key (user_id)
-references todo_user (id);
-
-alter table tasks add column priority_id int
-references priorities(id);
---
+++ db/scripts/004_ddl_alter_tasks.sql
@@ -0,0 +1,13 @@
+alter table tasks add column user_id int;
+
+alter table tasks
+add constraint todo_user_user_id_fkey
+foreign key (user_id)
+references todo_user (id);
+
+alter table tasks add column priority_id int;
+
+alter table tasks
+add constraint tasks_priority_id_fkey
+foreign key (priority_id)
+references priorities (id);
